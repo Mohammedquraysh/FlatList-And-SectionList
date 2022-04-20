b730fe102d5df03a1d9c220ea33f554a4a8c9155
mac <mohammedquraysh4@gmail.com>
2022-04-20 21:19:13 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 export default function App() {
+
+  const[Item, setItem] = useState([
+    { Item: 'item 1'},
+    {Item: 'item 2'},
+    {Item: 'item 3'},
+    {Item: 'item 4'},
+    {Item: 'item 5'},
+    {Item: 'item 6'},
+    {Item: 'item 7'},
+    {Item: 'item 8'},
+    {Item: 'item 9'}
+  ])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+    horizontal
+    inverted
+    keyExtractor={(item, index)=>index.toString}
+    data={Item}
+    renderItem={({item})=>(
+      <View style={styles.item}>
+       <Text style={styles.text}>{item.Item} </Text>
+      </View>
+    )}/>
+    // <ScrollView style={styles.container}>
+    //   {
+    //     Item.map((i)=>{
+    //       return(
+    //         <View style={styles.item}>
+    //           <Text style={styles.text}>{i.Item} </Text>
+    //         </View>
+    //       )
+    //     })
+    //   }
+    // </ScrollView>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#00ffff', 
+    flexDirection: 'column'
+    
   },
+  item:{
+    backgroundColor: '#000',
+    margin: 50,
+    justifyContent:'center',
+    alignItems: 'center'
+    
+  },
+  text:{
+    color: '#fff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 10
+  }
 });
